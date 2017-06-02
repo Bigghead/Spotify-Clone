763922df4fafd0d8a7ca278f4d28fd6231e57d4d
Shav <shavenchew@yahoo.com>
2017-06-01 22:18:36 -0700

    Playing around with the insanely complicated Spotify API

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
+var SpotifyWebApi = require('spotify-web-api-node');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+// var index = require('./routes/index');
+// var users = require('./routes/users');
 
 var app = express();
 
@@ -21,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, './client/dist')));
+app.use(cors());
 
 // app.use('/', index);
 // app.use('/users', users);
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http'
+import { Keys } from '../Keys'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor(private http: Http){}
+
   title = 'app works!';
+
+  ngOnInit(){
+    this.http.get(`https://accounts.spotify.com/authorize?client_id=${Keys.spotId}&response_type=token&redirect_uri=${Keys.callback}`)
+             .subscribe(
+               (res) => res
+             )
+  }
 }
