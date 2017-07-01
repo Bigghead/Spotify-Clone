4e7efe60b288040bac92930983be6ca7022a58bc
Shav <shavenchew@yahoo.com>
2017-07-01 13:21:50 -0700

    Mobile search

--- client/src/app/main-container/main-container.component.ts
+++ client/src/app/main-container/main-container.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../Services/authentication.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainContainerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
--- client/src/app/main-container/sidebar/sidebar.component.ts
+++ client/src/app/main-container/sidebar/sidebar.component.ts
@@ -1,3 +1,4 @@
+import { MusicPlayerService } from './../../Services/musicPlayer.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private musicPlayer: MusicPlayerService) { }
 
   ngOnInit() {
   }
